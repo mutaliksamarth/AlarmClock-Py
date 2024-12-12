31358b004b9df4f2e7c8ae9761d885cfcce6924d
mutaliksamarth <mutaliksamarth.02@gmail.com>
2024-12-12 15:09:51 +0530

    Completed Alarm Clock

---
+++ main.py
@@ -0,0 +1,30 @@
+# Python Alarm Clock
+
+import time
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "alarm.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+        if current_time == alarm_time:
+            print(f"Current Time: {current_time}")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                print("Time to wake up! Stupid!")
+                continue
+            break
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+    alarm_time = input("Enter the time for the alarm in format 'HH:MM:SS': ")
+    set_alarm(alarm_time)
